cb9fa5b37035b5c96478b5be80d8806b73aae6d1
shana <shana.cheon@betree.co.kr>
2019-08-27 17:52:17 +0900

    template upload

---
+++ 보리보리/js/common.js
@@ -0,0 +1,22 @@
+var bori = window.bori || {};
+
+    bori = (function($){
+    "use strict";
+console.log('asgsag');
+    var common = {
+
+
+        init : function() {
+
+        }
+    };
+
+    $(document).ready(function() {
+        common.init();
+    });
+
+    return {
+
+    }
+
+})(jQuery);
